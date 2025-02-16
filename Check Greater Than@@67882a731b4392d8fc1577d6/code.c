4b39d1f2ca309b64f6b83a3fcbff391bbd136260
Arshdeep Kaur <arshheer781@gmail.com>
2025-02-16 22:41:02 +0530

    Solution to Check Greater Than

--- Check Greater Than@@67882a731b4392d8fc1577d6/code.c
+++ Check Greater Than@@67882a731b4392d8fc1577d6/code.c
@@ -1,14 +1,15 @@
-#include<stdio.h>
-
-
-int main(){
+#include <stdio.h>
+int main()
+{
     int a,b;
     scanf("%d %d",&a,&b);
-    if(a>b){
-        return "True";
+    if(a>b)
+    {
+        printf("True");
     }
-    else{
-        return "False";
+    else
+    {
+        printf("False");
     }
     return 0;
 }
