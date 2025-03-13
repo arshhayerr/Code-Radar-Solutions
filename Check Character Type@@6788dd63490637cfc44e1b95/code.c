66c841a27d294594022d724b63bc50ca3caff47a
Arshdeep Kaur <arshheer781@gmail.com>
2025-03-14 00:52:32 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -1,22 +1,46 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int welcome(char c) {
-    if(c=='a' ||c=='e' || c=='i' || c=='o' || c=='u' ||
-    c=='A' || c=='E' || c=='I' || c=='O' || c=='U'){
-        return "Vowel";
-    }else if((c>='a' && c<='z') ||(c>='A' && c<='Z') ){
-        return "Consonant";
-    }else if (c>='0' && c<='9'){
-        return "Digit";
+// int welcome(char c) {
+//     if(c=='a' ||c=='e' || c=='i' || c=='o' || c=='u' ||
+//     c=='A' || c=='E' || c=='I' || c=='O' || c=='U'){
+//         return "Vowel";
+//     }else if((c>='a' && c<='z') ||(c>='A' && c<='Z') ){
+//         return "Consonant";
+//     }else if (c>='0' && c<='9'){
+//         return "Digit";
+//     }
+//     else{
+//         return "Special Character";
+//     }
+//     return 0;
+// }
+// int main() {
+//     char x;
+//     scanf("%c",&x);
+//     printf("%s", welcome(x));
+//     return 0;
+// } 
+#include <stdio.h>
+int main()
+{
+    char c;
+    scanf("%c",&c);
+    if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+    {
+        printf("Vowel");
     }
-    else{
-        return "Special Character";
+    else if(c=='b'||c=='c'||c=='d'||c=='f'||c=='g'||c=='h'||c=='j'||c=='k'||c=='l'||c=='m'||c=='n'||c=='p'||c=='q'||c=='r'||c=='s'||c=='t'||c=='v'||c=='w'||c=='x'||c=='y'||c=='z'||(c>='A'&& c<='Z'))
+    {
+        printf("Consonant");
+    }
+    else if(c=='0'||c=='1'||c=='2'||c=='3'||c=='4'||c=='5'||c=='6'||c=='7'||c=='8'||c=='9')
+    {
+        printf("Digit");
+    }
+    else 
+    {
+        printf("Special Character");
     }
     return 0;
+
 }
-int main() {
-    char x;
-    scanf("%c",&x);
-    printf("%s", welcome(x));
-    return 0;
-} 
