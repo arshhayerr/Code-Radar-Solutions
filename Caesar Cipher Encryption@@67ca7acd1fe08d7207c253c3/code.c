a56324921056b9bd7f8226ddd86a93c4aa6e63a1
Arshdeep Kaur <arshheer781@gmail.com>
2025-03-31 00:25:18 +0530

    Solution to Caesar Cipher Encryption

--- Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -1,10 +1,42 @@
-void caesarCipher(char message[] , int shift , char encrypted[]){
+#include <stdio.h>
 
-    int i=0;
-    while (message[i] !='\0'){
-        char l = message[i] + shift;
-        encrypted[i] = l;
+// Function to encrypt using Caesar Cipher
+void caesarCipher(char message[], int shift, char encrypted[]) {
+    int i = 0;
+
+    while (message[i] != '\0') {
+        char ch = message[i];
+
+        // Shift uppercase letters
+        if (ch >= 'A' && ch <= 'Z') {
+            encrypted[i] = ((ch - 'A' + shift) % 26) + 'A';
+        }
+        // Shift lowercase letters
+        else if (ch >= 'a' && ch <= 'z') {
+            encrypted[i] = ((ch - 'a' + shift) % 26) + 'a';
+        }
+        // Leave non-alphabet characters unchanged
+        else {
+            encrypted[i] = ch;
+        }
+
         i++;
     }
 
+    // Null-terminate the encrypted string
+    encrypted[i] = '\0';
+}
+
+// Driver code
+int main() {
+    char message[] = "Hello, World! 123";
+    int shift = 3;
+    char encrypted[100];
+
+    caesarCipher(message, shift, encrypted);
+
+    printf("Original Message: %s\n", message);
+    printf("Encrypted Message: %s\n", encrypted);
+
+    return 0;
 }
