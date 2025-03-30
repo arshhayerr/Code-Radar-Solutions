6595c748d871111389ec20a0e15fd93837e84a3d
Arshdeep Kaur <arshheer781@gmail.com>
2025-03-31 00:20:12 +0530

    Solution to Selection Sort for Integers

--- Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
+++ Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
@@ -1,35 +1,48 @@
-#include<stdio.h>
-#include<string.h>
-void swap(char str1[],char str2[])
-{
+#include <stdio.h>
+#include <string.h>
+
+// Function to swap two strings
+void swap(char str1[], char str2[]) {
     char temp[100];
-    strcpy(temp,str1);
-    strcpy(str1,str2);
-    strcpy(str2,temp);
+    strcpy(temp, str1);
+    strcpy(str1, str2);
+    strcpy(str2, temp);
 }
-void selectionSort(char arr[][100],int n)
-{
-    for(int i=0;i<n-1;i++)
-    {
-        int minIndex=i;
-        for(int j=i+1;j<n;j++)
-        {
-            if(strcmp(arr[j],arr[minIndex])<0)
-            {
-                minIndex=j;
+
+// Selection sort for an array of strings
+void selectionSort(char arr[][100], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (strcmp(arr[j], arr[minIndex]) < 0) {
+                minIndex = j;
             }
         }
-        if(minIndex!=i)
-        {
-            swap(arr[i],arr[minIndex]);
+        // Swap only if needed
+        if (minIndex != i) {
+            swap(arr[i], arr[minIndex]);
         }
     }
 }
-void printArray(char arr[][100],int n)
-{
-    for(int i=0;i<n;i++)
-    {
-        printf("%s\n",arr[i]);
+
+// Function to print an array of strings
+void printArray(char arr[][100], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%s\n", arr[i]);
     }
-   
+}
+
+int main() {
+    char arr[][100] = {"banana", "apple", "cherry", "blueberry", "mango"};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Before sorting:\n");
+    printArray(arr, n);
+
+    selectionSort(arr, n);
+
+    printf("\nAfter sorting:\n");
+    printArray(arr, n);
+
+    return 0;
 }
